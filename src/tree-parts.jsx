5b7ef32561d0ac411f767d25432e8b5741308fc3
Joshua Alfonso <joshua025icloud@gmail.com>
2024-11-21 17:50:46 +0800

    add wired tree

--- src/tree-parts.jsx
+++ src/tree-parts.jsx
@@ -12,7 +12,7 @@ const TreeParts = () => {
             </RoughNotation>
         </div>
 
-        <div className="mt-[80px] grid grid-cols-2">
+        <div className="mt-[80px] grid grid-cols-2 gap-4">
             <div className="flex flex-col items-center justify-center">
                 <img src="parol-nobg.png" width={200} height={200} className="object-cover"/>
                 <span className="text-[#FCFAEE] font-clouts tracking-widest text-2xl">Parol</span>
@@ -20,6 +20,10 @@ const TreeParts = () => {
             <div className="flex flex-col items-center justify-center">
                 <img src="lantern-nobg.png" width={200} height={200} className="object-cover"/>
                 <span className="text-[#FCFAEE] font-clouts tracking-widest text-2xl">Lantern</span>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+                <img src="wired-tree.png" width={200} height={200} className="object-cover"/>
+                <span className="text-[#FCFAEE] font-clouts tracking-widest text-2xl">Wired Tree</span>
             </div>
         </div>
         </>
