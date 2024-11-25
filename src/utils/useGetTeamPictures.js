b9aff5e3fe37d958d4334f031e35c1875024baf7
Joshua Alfonso <Joshua025icloud@gmail.com>
2024-11-25 19:56:36 +0800

    comment some pictures

--- src/utils/useGetTeamPictures.js
+++ src/utils/useGetTeamPictures.js
@@ -4,10 +4,10 @@ import image2 from '../assets/working-img/IMG_6548.jpeg';
 import image3 from '../assets/working-img/IMG_6549.jpeg';
 import image4 from '../assets/working-img/IMG_6551.jpeg';
 import image5 from '../assets/working-img/IMG_6557.jpeg';
-import image6 from '../assets/working-img/IMG_6574.jpeg';
-import image7 from '../assets/working-img/IMG_6576.jpeg';
-import image8 from '../assets/working-img/IMG_6579.jpeg';
-import image9 from '../assets/working-img/IMG_6554.jpeg';
+// import image6 from '../assets/working-img/IMG_6574.jpeg';
+// import image7 from '../assets/working-img/IMG_6576.jpeg';
+// import image8 from '../assets/working-img/IMG_6579.jpeg';
+// import image9 from '../assets/working-img/IMG_6554.jpeg';
 
 
 
@@ -18,10 +18,10 @@ const useImages = () => {
         { src: image3, alt: 'Image 3' },
         { src: image4, alt: 'Image 4' },
         { src: image5, alt: 'Image 5' },
-        { src: image6, alt: 'Image 6' },
-        { src: image7, alt: 'Image 7' },
-        { src: image8, alt: 'Image 8' },
-        { src: image9, alt: 'Image 9' },
+        // { src: image6, alt: 'Image 6' },
+        // { src: image7, alt: 'Image 7' },
+        // { src: image8, alt: 'Image 8' },
+        // { src: image9, alt: 'Image 9' },
 
         // { src: image6, alt: 'Image 6' },/
     ])
